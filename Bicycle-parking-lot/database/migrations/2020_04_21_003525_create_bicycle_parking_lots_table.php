5ce9901d39c5823636ddf288f22faf57d63baa85
川口博哉 <kawaguchihiroya1117@gmail.com>
2020-04-21 10:28:53 +0900

    データベース作成しテーブル挿入
    #1

---
+++ Bicycle-parking-lot/database/migrations/2020_04_21_003525_create_bicycle_parking_lots_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBicycleParkingLotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bicycle_parking_lots', function (Blueprint $table) {
+            $table->bigIncrements('bicycle_parking_lots_id')->comment('駐輪場コード');
+            $table->integer('company_id')->comment('事業者コード');
+            $table->integer('line_id')->comment('路線コード');
+            $table->integer('station_id')->comment('駅コード');
+            $table->string('bicycle_parking_lots_name')->comment(' 駐輪場名');
+            $table->integer('price')->comment('料金');
+            $table->string('street_adress')->nullable()->comment('住所');
+            $table->time('utilization_time')->nullable()->comment('利用時間');
+            $table->time('reception_time')->nullable()->comment('受付時間');
+            $table->integer('on_foot')->comment('徒歩');
+            $table->string('image_name')->nullable()->comment('画像url');
+            $table->string('image_url')->nullable()->comment('画像url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bicycle_parking_lots');
+    }
+}
